d02431f0b16eafcca1728b7fcc85fe80ea5e4d2e
muskan484 <72040045+muskan484@users.noreply.github.com>
2020-10-12 01:24:10 +0530

    Update SpeakHello.js

--- module-4 assignment/SpeakHello.js
+++ module-4 assignment/SpeakHello.js
@@ -1,10 +1,12 @@
-(function (window) {
-    var helloSpeaker = {};
-    var speakWord = "  Hello ";
-    helloSpeaker.speak = function (name) {
-        var wordsWritten = speakWord + name;
-        document.write(wordsWritten.fontsize(8));
-        console.log(wordsWritten);
-    }
-    window.helloSpeaker = helloSpeaker;
-})(window);
+(function(){
+ var helloSpeaker = {};
+
+var speakWord = "Hello";
+helloSpeaker.speak = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+
+
+window.helloSpeaker = helloSpeaker;
+
+})();
