74db09fa0f28f0f83d30b8abcd57fed12b4f0ffc
muskan484 <72040045+muskan484@users.noreply.github.com>
2020-10-12 01:10:39 +0530

    Update script.js

--- module-4 assignment/script.js
+++ module-4 assignment/script.js
@@ -1,11 +1,20 @@
-(function (window) {
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+(function () {
+
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim", ];
+    for (var i = 0; i < names.length; i++) {
+        names[i].toLowerCase();
+
+        if (names[i].charAt(0) == "J") {
+            byeSpeaker.speak(names[i]) + names[i]
+
+
+        } else {
+            helloSpeaker.speak(names[i]) + names[i]
+
+        }
     }
-})(window);
+
+
+
+})();
