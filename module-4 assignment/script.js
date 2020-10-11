761f2514a7d1839ae664aed2b8194bce19489aa1
muskan484 <72040045+muskan484@users.noreply.github.com>
2020-10-12 00:50:08 +0530

    Update script.js

--- module-4 assignment/script.js
+++ module-4 assignment/script.js
@@ -1,13 +1,13 @@
 (function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i in names) {
-
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
-
-  if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
-  } else {
-     window.helloSpeaker.speak(names[i]);
-  }
-}
+for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+    
+        //here we apply if condition to check in array if name start with "j" then if condition is work otherwise else  condition is going to work.....
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })(window);
